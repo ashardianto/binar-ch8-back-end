650aa1bbad3a9d72a72fca5c91546d4ce582ce87
ashardianto <ashardian.to21@gmail.com>
2022-04-18 19:09:06 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const apiRouter = require('./server/routes')
-const errorHandler = require('./server/middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const apiRouter = require("./server/routes");
+const errorHandler = require("./server/middlewares/errorHandler");
+
+// SETUP YAML
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDoc = YAML.load("./api.yaml");
+
+const PORT = process.env.PORT || 4000;
 
 // middlewares
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(errorHandler)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
 
 /**
  * @Routes /api
  * entrypoint for all API routes
  */
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
